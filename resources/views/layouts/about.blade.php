378f3d22d1fef9f4d01305a819ccdc944452bf4f
GRadith <g.radith06@gmail.com>
2023-08-21 14:03:10 +0700

    update

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -10,7 +10,7 @@
 
         <div class="row">
           <div class="col-lg-4">
-            <img src="Kelly/assets/img/me.jpeg" class="img-fluid" alt="">
+            <img src="Kelly/assets/img/about.jpg" class="img-fluid" alt="">
           </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content">
             <h3>Radith Widianto</h3>
--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -6,7 +6,7 @@
 
         <div class="section-title">
           <h2>Contact</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>Pada halaman ini saya akan menjelaskan lebih detail tentang contact person saya, serta anda dapat mengirim pesan email kepada saya jika anda membutuhkannya.</p>
         </div>
 
         <div>
@@ -20,19 +20,19 @@
               <div class="address">
                 <i class="bi bi-geo-alt"></i>
                 <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>Bumi Candi Asri, Candi, Sidoarjo, Jawa Timur, Indonesia</p>
               </div>
 
               <div class="email">
                 <i class="bi bi-envelope"></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>gabriel06@gmail.com</p>
               </div>
 
               <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <p>089617309258</p>
               </div>
 
             </div>
@@ -41,29 +41,28 @@
 
           <div class="col-lg-8 mt-5 mt-lg-0">
 
-            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-              <div class="row">
-                <div class="col-md-6 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-                </div>
-                <div class="col-md-6 form-group mt-3 mt-md-0">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-                </div>
-              </div>
-              <div class="form-group mt-3">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-              </div>
-              <div class="form-group mt-3">
-                <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-              </div>
-              <div class="my-3">
-                <div class="loading">Loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">Your message has been sent. Thank you!</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            </form>
-
+          <form action="https://formspree.io/f/mjvqyzew" method="POST">
+            <div class="form-group mt-3">
+            <h5>Email<span class="req">*</span></h5>
+              <input type="text" class="form-control" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="form-group mt-3">
+            <h5>Name<span class="req">*</span></h5>
+              <input type="text" class="form-control" name="name" id="name" placeholder="Name" required>
+            </div>
+            <div class="form-group mt-3">
+            <h5>Subject<span class="req">*</span></h5>
+              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+            </div>
+            <label class="msg">
+              <h5>Message<span class="req">*</span></h5>
+              <textarea name="message" placeholder="  Hi!" required></textarea>
+            </label>
+            <!-- your other form fields go here -->
+            <div class="btn"> 
+              <button type="submit">Send</button>
+            </div>
+          </form>
           </div>
 
         </div>
